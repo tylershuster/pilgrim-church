915cc7f24abdaa661b545e0ac19c3d3c8e94bc74
Tyler Shuster <tyler@shuster.house>
2015-12-18 11:05:58 -0800

    Removing liturgical calendar due to timeout

--- front-page.php
+++ front-page.php
@@ -16,26 +16,26 @@ echo do_shortcode('[upcoming_events]');
 
 echo "</div>";
 
-
-$json = file_get_contents("http://calapi.inadiutorium.cz/api/v0/en/calendars/default/today");
-
-$json = json_decode( $json );
+//
+// $json = file_get_contents("http://calapi.inadiutorium.cz/api/v0/en/calendars/default/today");
+//
+// $json = json_decode( $json );
 
 ?>
 
 <style media="screen">
 	.home__liturgical-info {
-		border-color: <?php echo $json->celebrations[0]->colour; ?>
+		border-color: <?php //echo $json->celebrations[0]->colour; ?>
 	}
 </style>
 
 <?php
 
-echo "<div class='home__liturgical-info'>";
-
-	echo "We are in the " . ordinal( $json->season_week ) . " week of " . $json->season . "!";
-
-echo "</div>";
+// echo "<div class='home__liturgical-info'>";
+//
+// 	echo "We are in the " . ordinal( $json->season_week ) . " week of " . $json->season . "!";
+//
+// echo "</div>";
 
 get_footer();
  ?>
